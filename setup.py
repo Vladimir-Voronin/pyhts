9a67f6b8aae4c6eeabd843b7de7f1a396c473d7a
Воронин Владимир <vladimirvoron7@gmail.com>
2022-07-09 17:17:33 +0300

    add setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyhts',
     version='1.0',
-    description='module for HTS.app (recognition and text handling)',
+    description='module for HTS app (recognition and text handling)',
     author='Vladimir Voronin',
     packages=['pyhts'],  # same as name
     install_requires=[],  # external packages as dependencies
